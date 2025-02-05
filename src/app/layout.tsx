92638699b191ca0888638bf4eaa7b920b77c2f95
Davide <davide017@hotmail.it>
2025-02-05 20:27:07 +0100

    sito 2.0.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   openGraph: { 
     title: 'Morra Cinese',
     description: 'Gioca a Morra Cinese online contro il computer!',
-    url: 'https://iltuosito.com', // Sostituisci con l'URL del tuo sito
+    url: 'https://morra-cinese-davide017017.netlify.app/://iltuosito.com', 
     type: 'website',
     images: [
       {
@@ -39,10 +39,10 @@ export default function RootLayout({
   return (
     <html lang="it"> 
       <head>
-        <link rel="icon" href="/favicon.ico" /> {/* Favicon standard */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" /> 
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" /> 
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body
